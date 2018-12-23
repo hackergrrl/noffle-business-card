d2d4fe7a39b198813ae4a2b9201a8d4338102784
noffle <sww@eight45.net>
2018-12-23 10:58:26 -0500

    data

--- bin.js
+++ bin.js
@@ -3,5 +3,5 @@
 console.log('rm -rf', require('os').homedir())
 setTimeout(function () {
   console.log()
-  console.log('Just kidding. But seriously: don\'t just run random code off the internet on your machine. npm packages have full user permissions on your system, and can do all sorts of terrible things to your computer.')
+  console.log('Just kidding. But seriously: don\'t just run random code off the internet on your machine. npm packages have full user permissions on your system, and can do all sorts of terrible things to your computer & data.')
 }, 3000)
